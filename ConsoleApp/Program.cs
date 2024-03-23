e132f815fceee5165fb2128aa41a610292cf4d45
s20178 <s20178@pjwstk.edu.pl>
2024-03-23 16:45:11 +0100

    del

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -30,7 +30,6 @@ namespace ConsoleApp
             foreach (int num in array)
             {
                 sum += num;
-                Console.WriteLine(sum);
             }
 
             return (double)sum / array.Length;
